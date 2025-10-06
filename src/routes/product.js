91aa0da0432ed349422d99ea149e44fd78cad638
ThomasBrets <thomas15501331@gmail.com>
2025-10-06 12:39:27 -0300

    fix middleware admin

--- src/routes/product.js
+++ src/routes/product.js
@@ -2,16 +2,19 @@ import { Router } from "express";
 const router = Router();
 import { productController } from "../controllers/productController.js";
 import { verifyRole } from "../middlewares/verifyRole.js";
+import { passportCall } from "../middlewares/passport/passport-call.js";
 
 import admin from "./admin.js";
-
 
 router.get("/", productController.getAll);
 router.get("/:pid", productController.getById);
 
 //! Admin
-router.use("/admin",verifyRole("admin"), admin);
-
+router.use(
+  "/admin",
+  passportCall("jwt", { session: false }),
+  verifyRole("ADMIN"),
+  admin
+);
 
 export default router;
-
